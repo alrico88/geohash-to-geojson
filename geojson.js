50cde15d7a6dd6fcc2b64a7ad9342804c2b54460
Alberto Rico <alrico88@gmail.com>
2021-02-21 22:45:44 +0100

    Using OOP and inheritance

--- geojson.js
+++ geojson.js
@@ -1,31 +1,20 @@
-/**
- * Point class
- *
- * @class Point
- */
-class Point {
+class Feature {
 
   /**
-   * Creates an instance of Point.
-   * @param {number} latitude
-   * @param {number} longitude
-   * @param {object} [properties={}]
-   * @memberof Point
+   * Creates a new Feature
+   * @param {object} geometry
+   * @param {object} properties
    */
-  constructor(latitude, longitude, properties = {}) {
+  constructor(geometry, properties) {
     this.type = 'Feature';
-    this.geometry = {
-      type: 'Point',
-      coordinates: [longitude, latitude],
-    };
+    this.geometry = geometry;
     this.properties = properties;
   }
 
   /**
-   * Gets content as plain object
-   *
-   * @returns {{type: 'Feature', geometry: {type: 'Point', coordinates: number[]}, properties: object}}
-   * @memberof Point
+   * Returns plain object representation
+   * @return {{geometry: (*|{type: string, coordinates: number[]}|{type: "Point", coordinates: number[]}|{type: string, coordinates: number[][][]}|{type: "Polygon", coordinates: number[][][]}), type, properties: any}}
+   * @memberof Feature
    */
   toPlain() {
     return {
@@ -37,11 +26,36 @@ class Point {
 }
 
 /**
+ * Point class
+ *
+ * @class Point
+ */
+class Point extends Feature {
+
+  /**
+   * Creates an instance of Point.
+   * @param {number} latitude
+   * @param {number} longitude
+   * @param {object} [properties={}]
+   * @memberof Point
+   */
+  constructor(latitude, longitude, properties = {}) {
+    super(
+      {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      },
+      properties
+    );
+  }
+}
+
+/**
  * PolygonFromBBox class
  *
  * @class PolygonFromBBox
  */
-class PolygonFeatureFromBBox {
+class PolygonFeatureFromBBox extends Feature {
 
   /**
    * Creates an instance of PolygonFeatureFromBBox.
@@ -50,33 +64,20 @@ class PolygonFeatureFromBBox {
    * @memberof PolygonFeatureFromBBox
    */
   constructor(bbox, properties = {}) {
-    this.type = 'Feature';
-    this.properties = properties;
-    this.geometry = {
-      type: 'Polygon',
-      coordinates: (() => {
-        const [minLon, minLat, maxLon, maxLat] = bbox;
-        const sw = new Point(minLat, minLon).geometry.coordinates;
-        const nw = new Point(maxLat, minLon).geometry.coordinates;
-        const ne = new Point(maxLat, maxLon).geometry.coordinates;
-        const se = new Point(minLat, maxLon).geometry.coordinates;
-        return [[sw, se, ne, nw, sw]];
-      })(),
-    };
-  }
-
-  /**
-   * Gets content as plain object
-   *
-   * @returns {{type: 'Feature', geometry: {type: 'Polygon', coordinates: number[][][]}, properties: object}}
-   * @memberof PolygonFeatureFromBBox
-   */
-  toPlain() {
-    return {
-      type: this.type,
-      geometry: this.geometry,
-      properties: this.properties,
-    };
+    super(
+      {
+        type: 'Polygon',
+        coordinates: (() => {
+          const [minLon, minLat, maxLon, maxLat] = bbox;
+          const sw = new Point(minLat, minLon).geometry.coordinates;
+          const nw = new Point(maxLat, minLon).geometry.coordinates;
+          const ne = new Point(maxLat, maxLon).geometry.coordinates;
+          const se = new Point(minLat, maxLon).geometry.coordinates;
+          return [[sw, se, ne, nw, sw]];
+        })(),
+      },
+      properties
+    );
   }
 }
 
@@ -95,7 +96,7 @@ class FeatureCollection {
   /**
    * Gets content as plain object
    *
-   * @returns {{type: 'FeatureCollection', features: object[]}
+   * @returns {{type: 'FeatureCollection', features: object[]}}
    * @memberof FeatureCollection
    */
   toPlain() {
